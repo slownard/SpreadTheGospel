e22146225c70df89593d088db8075d3cf3404c6f
Bernard Legaspi <blegaspi26@gmail.com>
2023-08-07 16:14:27 -0400

    In the beginning

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+// create page 
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    EVANGELISM ACROSS THE NATION
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
