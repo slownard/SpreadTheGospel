dd2357fc8eca91a4d3dc2f760fe634f4872aadf1
Bernard Legaspi <blegaspi26@gmail.com>
2023-08-12 14:54:10 -0400

    putting in that work

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,30 @@
 // create page 
 // import logo from './logo.svg';
 // import './App.css';
+import React, { useState, useEffect } from "react";
 
 
 function App() {
+
+    // set your state
+    const [churches, setChurches] = useState([])
+
+    // 
+    useEffect(() => {
+        fetch('http://localhost:3001/churches')
+            .then(res => res.json())
+            .then((data) => setChurches(data));
+    }, [])
+
+
     return (
         <div className="App">
             <header className="App-header">
                 <p>
                     EVANGELISM ACROSS THE NATION
                 </p>
+
+
             </header>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
