c1f4d6b8186b68bacfde4e36075d6d28cb68675e
Bernard Legaspi <blegaspi26@gmail.com>
2023-08-16 13:13:36 -0400

    working on header

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@
 // import logo from './logo.svg';
 // import './App.css';
 import React, { useState, useEffect } from "react";
+import Header from "./Header";
+
 
 
 function App() {
@@ -9,12 +11,12 @@ function App() {
     // set your state
     const [churches, setChurches] = useState([])
 
-    // 
-    useEffect(() => {
-        fetch('http://localhost:3001/churches')
-            .then(res => res.json())
-            .then((data) => setChurches(data));
-    }, [])
+    // fetching json
+    // useEffect(() => {
+    //     fetch('http://localhost:3001/churches')
+    //         .then(res => res.json())
+    //         .then((data) => setChurches(data));
+    // }, [])
 
 
     return (
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+function Header() {
+
+    return (
+        <>
+
+        </>
+
+    )
+}
+
+export default Header;
