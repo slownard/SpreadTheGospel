8ed979d952464367d8067e52113fa4dca4b19fc1
Bernard Legaspi <blegaspi26@gmail.com>
2023-09-06 15:41:11 -0400

    added form stuff

--- src/components/Submitform.js
+++ src/components/Submitform.js
@@ -2,12 +2,64 @@ import React, { useState, useEffect } from "react";
 
 function Submitform({ handleAddChurch }) {
 
+    const [name, setName] = useState('')
+    const [address, setAddress] = useState('')
+    const [denomination, setDenomination] = useState('')
+    const [pastor, setPastor] = useState('')
+    const [iamge, setImage] = useState('')
 
+    function handleDubmit(e) {
+        e.preventDefautl()
 
+        const newChurch = {
+            name,
+            address,
+            denomination,
+            pastor,
+            image
+        }
+        handleAddChurch(newChurch)
+    }
 
 
     return (
         <>
+
+            <h1>New Church</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                />
+                <input
+                    onChange={(e) => setAddress(e.target.value)}
+                    type="text"
+                    name="address"
+                    placeholder="Address"
+                />
+                <input
+                    onChange={(e) => setDenomination(e.target.value)}
+                    type="text"
+                    name="denomination"
+                    placeholder="denomination"
+                />
+                <input
+                    onChange={(e) => setPastor(e.target.value)}
+                    type="text"
+                    name="pastor"
+                    placeholder="pastor"
+                />
+                <input
+                    onChange={(e) => setImage(e.target.value)}
+                    type="text"
+                    name="image"
+                    placeholder="Image URL"
+                />
+                <button type="submit">Add church</button>
+            </form>
         </>
     )
 }
